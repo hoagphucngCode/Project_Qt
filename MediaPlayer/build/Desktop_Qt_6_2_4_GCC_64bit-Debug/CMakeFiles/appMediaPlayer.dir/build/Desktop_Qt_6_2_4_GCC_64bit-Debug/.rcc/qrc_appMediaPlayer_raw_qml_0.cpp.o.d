5d1f9311dc2ae08db0603563ca29ad40c8c55780
trus2lun <trus2lun@gmail.com>
2024-07-16 10:21:18 +0700

    update

--- MediaPlayer/build/Desktop_Qt_6_2_4_GCC_64bit-Debug/CMakeFiles/appMediaPlayer.dir/build/Desktop_Qt_6_2_4_GCC_64bit-Debug/.rcc/qrc_appMediaPlayer_raw_qml_0.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/appMediaPlayer.dir/build/Desktop_Qt_6_2_4_GCC_64bit-Debug/.rcc/qrc_appMediaPlayer_raw_qml_0.cpp.o: \
- /home/fr/Downloads/MediaPlayer/MediaPlayer/build/Desktop_Qt_6_2_4_GCC_64bit-Debug/.rcc/qrc_appMediaPlayer_raw_qml_0.cpp \
- /usr/include/stdc-predef.h
